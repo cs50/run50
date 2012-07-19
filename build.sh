e26a1fd2ddbf04ddf707b9dc519475fcc02dc89a
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2012-07-19 16:57:12 -0400

    stupid yui, cat-ing files before build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf build
 mkdir -p build/img
 
 echo "Building JavaScript..."
-java -jar yuicompressor-2.4.7.jar -o build/cs50.run.min.js \
+cat \
     js/lib/jquery-ui-1.8.21.custom.min.js \
     js/lib/socket.io.min.js \
     js/lib/underscore-min.js \
@@ -24,15 +24,19 @@ java -jar yuicompressor-2.4.7.jar -o build/cs50.run.min.js \
     js/lib/codemirror/python.js \
     js/lib/codemirror/search.js \
     js/lib/codemirror/xml.js \
-    js/cs50.run.js
+    js/cs50.run.js \
+    > build/cs50.run.min.js
+java -jar yuicompressor-2.4.7.jar -o build/cs50.run.min.js build/cs50.run.min.js
 
 echo "Building CSS..."
-java -jar yuicompressor-2.4.7.jar -o build/cs50.run.min.css \
+cat \
     css/lib/jquery-ui-1.8.21.custom.css \
     css/lib/codemirror.css \
     css/lib/codemirror-dialog.css \
     css/lib/chosen/chosen.css \
-    css/cs50.run.css
+    css/cs50.run.css \
+    > build/cs50.run.min.css
+java -jar yuicompressor-2.4.7.jar -o build/cs50.run.min.css build/cs50.run.min.css
 
 echo "Copying images..."
 cp css/img/* build/img
