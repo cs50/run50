c2fe3bafd0f390eab09d1d061892df54cb163019
David J. Malan <malan@harvard.edu>
2012-09-01 11:57:39 -0400

    Changed -fcolor-diagnostics to -fno-color-diagnostics

--- js/cs50.run.js
+++ js/cs50.run.js
@@ -51,7 +51,7 @@ CS50.Run = function(options) {
     // map from mimes to commands necessary to run code
     this.commands = {
         'text/x-csrc': [
-            { command: 'clang file.c', args: '-std=c99 -Wall -Werror -fcolor-diagnostics -lcs50 -lm' },
+            { command: 'clang file.c', args: '-std=c99 -Wall -Werror -fno-color-diagnostics -lcs50 -lm' },
             { command: './a.out', args: '' }
         ],
 
